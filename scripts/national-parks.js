1225f5ec023870593d5626d0b4242bb2802e8857
nedretcelik <nedretcelik97@gmail.com>
2024-05-20 11:29:41 -0400

    add function to create state list

--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -1,6 +1,21 @@
+"use strict";
 
-function loadParks() {
-  for (const park of nationalParksArray) {
+window.onload = function () {
+  const nationalParksList = document.querySelector("#nationalParksList");
+
+  function loadParks() {
+    for (const park of nationalParksArray) {
+      ListOfState(park)
+    }
   }
-}
 
+  function ListOfState(parkData) {
+    let stateOption = new Option("option");
+    stateOption.value = parkData.State;
+    stateOption.innerText = parkData.State;
+
+    nationalParksList.appendChild(stateOption);
+  }
+
+  loadParks();
+};
