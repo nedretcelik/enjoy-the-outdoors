0ab72cd7135fa58b123f21da903e45257bef709f
nedretcelik <nedretcelik97@gmail.com>
2024-05-21 14:48:50 -0400

    add hide function for dropdown when user clicks view all parks option and show all parks

--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -5,6 +5,7 @@ window.onload = function () {
 
   const byLocationRadio = document.querySelector("#byLocationRadio");
   const byTypeRadio = document.querySelector("#byTypeRadio");
+  const viewAllParksOption = document.querySelector("#viewAllParksOption");
 
   const parksDetailDiv = document.querySelector("#parksDetailDiv");
 
@@ -14,6 +15,7 @@ window.onload = function () {
     parksDetailDiv.innerText = "";
 
     if (byLocationRadio.checked) {
+      nationalParksList.style.display = "block";
       let newOption = new Option("option");
       newOption.innerText = "Select Location";
 
@@ -23,6 +25,7 @@ window.onload = function () {
         buildSearchOptionState(park);
       }
     } else if (byTypeRadio.checked) {
+      nationalParksList.style.display = "block";
       let newOption = new Option("option");
       newOption.innerText = "Select Park Type";
 
@@ -30,6 +33,13 @@ window.onload = function () {
       parkTypesArray.sort();
       for (const type of parkTypesArray) {
         buildSearchOptionType(type);
+      }
+    }
+
+    if (viewAllParksOption.checked) {
+      nationalParksList.style.display = "none";
+      for (const park of locationsArray) {
+        showAllParks(park)
       }
     }
   }
@@ -48,6 +58,50 @@ window.onload = function () {
     parkTypeOption.innerText = parkData;
 
     nationalParksList.appendChild(parkTypeOption);
+  }
+
+  function showAllParks(parkData) {
+    for (const park of nationalParksArray) {
+        let cardDiv = document.createElement("div");
+        cardDiv.classList.add("card", "mt-5", "custom-card", "d-flex", "justify-content-center");
+        parksDetailDiv.appendChild(cardDiv);
+
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        cardDiv.appendChild(cardBody);
+
+        let locationNameh4 = document.createElement("h4");
+        locationNameh4.innerText = park.LocationName;
+        cardBody.appendChild(locationNameh4);
+
+        let locationNameh5 = document.createElement("h5");
+        locationNameh5.innerText = "Adress: ";
+        cardBody.appendChild(locationNameh5);
+
+        let addressP = document.createElement("p");
+        addressP.innerText = `${park.Address}`;
+        cardBody.appendChild(addressP);
+
+        let locationP = document.createElement("p");
+        locationP.innerText = `${park.City}, ${park.State} ${park.ZipCode}`;
+        cardBody.appendChild(locationP);
+
+        let phoneP = document.createElement("p");
+        phoneP.innerText = `Phone: ${park.Phone}`;
+        cardBody.appendChild(phoneP);
+
+        let faxP = document.createElement("p");
+        faxP.innerText = `Fax: ${park.Fax}`;
+        cardBody.appendChild(faxP);
+
+        if (park.Visit) {
+          let visitLinkP = document.createElement("a");
+          visitLinkP.href = ` ${park.Visit}`;
+          visitLinkP.innerText = `Visit: ${park.Visit}`;
+          cardBody.appendChild(visitLinkP);
+        }
+    }
   }
 
   function nationalParksDetailCard() {
@@ -88,20 +142,19 @@ window.onload = function () {
         faxP.innerText = `Fax: ${park.Fax}`;
         cardBody.appendChild(faxP);
 
-       if(park.Visit) {
-        let visitLinkP = document.createElement("a");
-        visitLinkP.href = ` ${park.Visit}`;
-        visitLinkP.innerText = `Visit: ${park.Visit}`;
-        cardBody.appendChild(visitLinkP);
-       }
-
-
+        if (park.Visit) {
+          let visitLinkP = document.createElement("a");
+          visitLinkP.href = ` ${park.Visit}`;
+          visitLinkP.innerText = `Visit: ${park.Visit}`;
+          cardBody.appendChild(visitLinkP);
+        }
       }
     }
   }
 
   byLocationRadio.onchange = loadParks;
   byTypeRadio.onchange = loadParks;
+  viewAllParksOption.onchange = loadParks;
 
   nationalParksList.onchange = nationalParksDetailCard;
 };
