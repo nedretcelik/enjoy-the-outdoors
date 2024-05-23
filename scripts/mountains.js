370c4b2e258f9a396da75ddb60c9343b57ee3120
nedretcelik <nedretcelik97@gmail.com>
2024-05-22 22:27:44 -0400

    update styles

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -28,38 +28,46 @@ window.onload = function () {
         let cardDiv = document.createElement("div");
         cardDiv.classList.add("card", "mt-5", "custom-card-m", "d-flex", "justify-content-center");
         mountainDetailsDiv.appendChild(cardDiv);
-  
+
         let cardBody = document.createElement("div");
-        cardBody.classList.add("card-body","cardb-bg","justify-content-center");
+        cardBody.classList.add("card-body", "cardb-bg", "justify-content-center");
         cardDiv.appendChild(cardBody);
-  
+
         let mountainImage = document.createElement("img");
         mountainImage.src = mountain.img;
         mountainImage.alt = mountain.name;
-  
+
         cardBody.appendChild(mountainImage);
-  
+
         let mountainNameH5 = document.createElement("h5");
         mountainNameH5.innerText = mountain.name;
-  
+
         cardBody.appendChild(mountainNameH5);
-  
+
         let mountainDescP = document.createElement("p");
         mountainDescP.innerText = "Description: " + mountain.desc;
-  
+
         cardBody.appendChild(mountainDescP);
-  
+
         let mountainElevationP = document.createElement("p");
-        mountainElevationP.innerText = "Elevation: "+ mountain.elevation;
-  
+        mountainElevationP.innerText = "Elevation: " + mountain.elevation;
+
         cardBody.appendChild(mountainElevationP);
+
+        getSunsetForMountain(mountain.coords.lat, mountain.coords.lng).then((data) => {
+          console.log(data.results);
+        });
       }
     }
-      
-    }
-   
+  }
 
-  
+  //function that can "fetch" the sunrise/sunset times
+  async function getSunsetForMountain(lat, lng) {
+    let response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
+    let data = await response.json();
+    return data;
+  }
+
   loadMountains();
   mountainsList.onchange = showMountainDetails;
 };
